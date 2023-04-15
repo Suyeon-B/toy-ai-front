525200fd2a5e577186a10a594720b93afb0ee76c
Suyeon-B <tndus4243@naver.com>
2023-04-15 14:28:33 +0900

    chore: set layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="max-w-screen-md mx-auto px-20">
+      <Component {...pageProps} />
+    </div>
+  );
 }
