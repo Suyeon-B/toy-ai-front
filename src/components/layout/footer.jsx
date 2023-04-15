dcbd042983e88123aba1c38c7002aee2d3711407
Suyeon-B <tndus4243@naver.com>
2023-04-15 15:45:45 +0900

    feature: layout component

---
+++ src/components/layout/footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer class="bg-gray-300 py-4 mt-auto">
+      <div class="container mx-auto px-4 sm:px-6 lg:px-8 flex justify-center items-center">
+        <span class="text-gray-600 text-sm">
+          &copy; 2023 책너두 All rights reserved.
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
