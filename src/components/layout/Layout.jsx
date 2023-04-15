6942b61f91066e36c9aaec4b773d7c743ef0451e
Suyeon-B <tndus4243@naver.com>
2023-04-15 17:25:29 +0900

    delete: layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -9,11 +9,7 @@ const Layout = ({ children }) => {
         <title>킹너두</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <div class="flex flex-col min-h-screen bg-gray-100 text-black">
-        <Header />
-        {children}
-        <Footer />
-      </div>
+      {children}
     </>
   );
 };
