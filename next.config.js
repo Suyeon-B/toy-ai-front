64a0a6146c7cdf382fcce4a469376f64b8e8c37b
Suyeon-B <tndus4243@naver.com>
2023-04-18 02:31:17 +0900

    add: build config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,10 @@
 /** @type {import('next').NextConfig} */
-// eslint-disable-next-line no-unused-vars
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-// eslint-disable-next-line no-undef
 module.exports = {
   async rewrites() {
     return [
       {
         source: "/:path*",
-        destination: " :path*",
+        destination: "http://54.180.88.28:8080/:path*",
       },
     ];
   },
