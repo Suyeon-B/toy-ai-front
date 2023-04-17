026897950b2d1da60ae0e4030b819c38308a97c9
Suyeon-B <tndus4243@naver.com>
2023-04-18 02:47:32 +0900

    push

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
-export async function rewrites() {
-  return [
-    {
-      source: "/:path*",
-      destination: "http://54.180.88.28:8080/:path*",
-    },
-  ];
-}
+// eslint-disable-next-line no-undef
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "http://54.180.88.28:8080/:path*",
+      },
+    ];
+  },
+};
