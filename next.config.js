2f51c5267b328366a2bbd3ce34e273d3f81a32e5
Suyeon-B <tndus4243@naver.com>
2023-04-18 02:44:31 +0900

    fix: build error

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: "http://54.180.88.28:8080/:path*",
-      },
-    ];
-  },
-};
+export async function rewrites() {
+  return [
+    {
+      source: "/:path*",
+      destination: "http://54.180.88.28:8080/:path*",
+    },
+  ];
+}
