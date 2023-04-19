c31f0c5f456d37354d55cdbbe67fbc33c9506967
Suyeon-B <tndus4243@naver.com>
2023-04-19 09:54:51 +0900

    feature: add footer link

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
@@ -5,7 +6,8 @@ const Footer = () => {
     <footer className="bg-slate-200 py-4 mt-auto">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex justify-center items-center">
         <span className="text-gray-600 text-sm">
-          &copy; 2023 책너두 All rights reserved.
+          &copy; 2023 <Link href="https://breakbook.oopy.io/">책너두</Link> All
+          rights reserved.
         </span>
       </div>
     </footer>
